67c6241768c36d5e513aa380238154a22285494f
OdiljOVLIYEV <konnormen@gmail.com>
2022-07-12 19:07:47 +0500

    1

---
+++ Assets/Horror/Script/flashOn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashOn : MonoBehaviour
+{
+	public GameObject fonar;
+	public bool flash;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    fonar.SetActive(false);
+	    
+    }
+
+    // Update is called once per frame
+	void Update()
+    {
+	    if(Input.GetKeyDown(KeyCode.F)){
+	    	if(flash==false){
+	    		flash=true;
+	    		fonar.SetActive(true);
+	    	}
+	    	else{
+	    		flash=false;
+	    		fonar.SetActive(false);
+	    	}
+	    	
+	    }
+	    
+    }
+}
--- Assets/Horror/Script/shoot.cs
+++ Assets/Horror/Script/shoot.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class shoot : MonoBehaviour
 {
@@ -10,23 +11,54 @@ public class shoot : MonoBehaviour
 	public Camera cam;
 	public ParticleSystem gun;
 	public AudioSource sound;
-    // Start is called before the first frame update
    
-
+	//o'qdori kodi
+	public int MAXammo=12;
+	//public int patron=6;
+	public int bullet;
+	public float time;
+	public bool tugadi;
+	public Text text;
+	
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start()
+	{
+		if(bullet==-1)
+		bullet=MAXammo;
+	}
     // Update is called once per frame
     void Update()
-    {
+	{
+		
+		
+		
 	    if(Input.GetButtonDown("Fire1")){
 	    	
 	    	Shoot();
-	    	
+	    	Ammo();
 	    }
-    }
-    
+	    
+		if(bullet<=0){
+			
+			Reloading();
+			return;
+		}
+		text.text=bullet.ToString();
+		if(Input.GetKeyDown(KeyCode.R)){
+			
+			
+		}
+				
+		
+	}
+	
+			
     
 	void Shoot(){
 		gun.Play();
 		sound.Play();
+		
+		bullet--;
 		RaycastHit hit;
 		if(Physics.Raycast(cam.transform.position,cam.transform.forward,out hit,range)){
 			
@@ -34,9 +66,23 @@ public class shoot : MonoBehaviour
 			Debug.Log(hit.transform);
 			
 			Target target	= hit.transform.GetComponent<Target>();
+			
 			if(target!=null){
 				target.TakeDamage(damage);
-			}
+				}
+				
+				
 		}
 	}
+	
+	void Ammo(){
+		
+		//patron-=1;
+		
+	}
+	void Reloading(){
+		
+		bullet=MAXammo; 
+	}
+	
 }
